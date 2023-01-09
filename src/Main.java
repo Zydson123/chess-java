61ccf7ca18dcd877c3648e386843ebcdd5c2eea8
Zydson <101566833+Zydson123@users.noreply.github.com>
2023-01-09 19:07:37 +0100

    smol fix in print board

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,16 @@ public class Main {
         }
     }
     public static void print_board(Piece board[][]){
-        for (int i = 0; i < board.length; i++)
+        int i =0;
+        int j = 0;
+        System.out.println();
+        System.out.print("  1    2   3   4   5   6   7   8");
+        System.out.println();
+        for (i = 0; i < board.length; i++)
         {
-            for (int j = 0; j < board.length; j++)
+            int kot = i+1;
+            System.out.print(kot);
+            for (j = 0; j < board.length; j++)
             {
                 System.out.print(" " + board[i][j] + " ");
             }
